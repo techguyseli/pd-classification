87b46dedaa28c4636a352d60418b3fe13dc7716a
techguyseli <kassouselaiman@gmail.com>
2023-08-14 13:57:24 +0100

    added and tested the instantaneous velocity extractor

--- datamanipulation/extractors.py
+++ datamanipulation/extractors.py
@@ -1,5 +1,5 @@
 """
-tangetial, horizontal and vertical: velocity, acceleration, jerk, snap, crackle, pop.
+tangetial, horizontal and vertical: acceleration, jerk, snap, crackle, pop.
 first derivative of pressure
 slope
 
@@ -50,28 +50,24 @@ class DisplacementExtractor:
 
     def _extract(self, data):
         """
-        Extract the displacement from the data dictionary (for one image).
+        Extract the displacement from the data dictionary (of one image) and add it to it.
 
         Args:
-            data (dict): Dictionary provided through the caller function in datamanipulation.extractors.extract_features.
-
-        Returns:
-            data (dict): The input dictionary, but updated, this is done to optimize performance and for programming logic. 
+            data (dict): Dictionary with keys as features and their matrices as values.
         """
         if data[self.name] is not None:
-            return data
+            return
 
         points = data['raw'][:, 1:3].copy()
         points = points[:,0:1] if self.mode == 'x' else points[:,1:2]
 
         points_i_minus1 = np.zeros(points.shape)
-        points_i_minus1[1:,:] = points[0:-1, :]
+        points_i_minus1[1:,:] = points[0:-1, :].copy()
 
         displacement = np.zeros(points.shape)
         displacement[1:,:] = DisplacementExtractor.calculate(points_i_minus1[1:,:], points[1:,:])
 
         data[self.name] = displacement
-        return data
 
 
 class DistanceExtractor:
@@ -123,16 +119,13 @@ class DistanceExtractor:
 
     def _extract(self, data):
         """
-        Extract the distance from the data dictionary (for one image).
+        Extract the distance from the data dictionary (of one image) and add it to it.
 
         Args:
-            data (dict): Dictionary provided through the caller function in datamanipulation.extractors.extract_features.
-
-        Returns:
-            data (dict): The input dictionary, but updated, this is done to optimize performance and for programming logic. 
+            data (dict): Dictionary with keys as features and their matrices as values.
         """
         if data[self.name] is not None:
-            return data
+            return
 
         points = data['raw'][:, 1:3].copy()
 
@@ -142,115 +135,140 @@ class DistanceExtractor:
             points = points[:,1:2]
 
         points_i_minus1 = np.zeros(points.shape)
-        points_i_minus1[1:,:] = points[0:-1, :]
-        distance = np.zeros((points.shape[0], 1))
+        points_i_minus1[1:,:] = points[0:-1, :].copy()
 
+        distance = np.zeros((points.shape[0], 1))
         if self.mode == 'xy':
             distance[1:,:] = DistanceExtractor.calculate_2d(
                 points_i_minus1[1:,0:1], 
                 points[1:,0:1],
                 points_i_minus1[1:,1:2], 
-                points[1:,1:2],
+                points[1:,1:2]
                 )
         else:
             distance[1:,:] = DistanceExtractor.calculate_1d(points_i_minus1[1:,:], points[1:,:])
             
         data[self.name] = distance
-        return data
 
 
-class VelocityExtractor:
-    def __init__(self, mode):
+class DurationExtractor:
+    def __init__(self):
         """
-        Initilize the mode for distance extraction.
-
-        Args:
-            mode (str): In 'x' or 'y' or 'xy'.
+        Initilize the name of the extractor.
         """
-        assert mode in ['x', 'y', 'xy'], "The mode for distance should be either 'x', 'y' or 'xy'."
-        self.mode = mode
-        self.name = 'distance_' + mode
+        self.name='duration'
 
 
     @classmethod
-    def calculate_2d(self, x1, x2, y1, y2):
+    def calculate(self, time1, time2):
         """
-        Calculate the Pythagorean distance between 2 points or vectors.
+        Calculate the duration between 2 timestamps.
 
         Args:
-            x1 (number or 2-D vector): The anterior points in the x-axis.
-            x2 (number or 2-D vector): The posterior points in the x-axis.
-            y1 (number or 2-D vector): The anterior points in the y-axis.
-            y2 (number or 2-D vector): The posterior points in the y-axis.
+            time1 (int): The first timestamp.
+            time2 (int): The second timestamp.
 
         Returns:
-            result (number or 2-D vector): The Pythagorean distance between (x1, y1) and (x2, y2).
+            result (int): The duration between time1 and time2.
         """
-        result = np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
-        return result
-
-
-    @classmethod
-    def calculate_1d(self, x1, x2):
-        """
-        Calculate the 1-D distance between 2 points or vectors.
-
-        Args:
-            x1 (number or 2-D vector): The anterior point(s).
-            x2 (number or 2-D vector): The posterior point(s).
-
-        Returns:
-            result (number or 2-D vector): The distance between x2 and x1.
-        """
-        result = np.abs(x1 - x2)
+        result = time2 - time1
         return result
 
 
     def _extract(self, data):
         """
-        Extract the distance from the data dictionary (for one image).
+        Extract the duration at each point from the data dictionary (of one image) and add it to it.
 
         Args:
-            data (dict): Dictionary provided through the caller function in datamanipulation.extractors.extract_features.
-
-        Returns:
-            data (dict): The input dictionary, but updated, this is done to optimize performance and for programming logic. 
+            data (dict): Dictionary with keys as features and their matrices as values.
         """
         if data[self.name] is not None:
-            return data
+            return
 
-        points = data['raw'][:, 1:3].copy()
+        time2 = data['raw'][:,0:1].copy()
+        time1 = np.zeros(time2.shape)
+        time1[1:,:] =  time2[:-1,:].copy()
+        duration = DurationExtractor().calculate(time1, time2)
+        data[self.name] = duration
 
-        if self.mode == 'x':
-            points = points[:,0:1]
-        elif self.mode == 'y':
-            points = points[:,1:2]
 
-        points_i_minus1 = np.zeros(points.shape)
-        points_i_minus1[1:,:] = points[0:-1, :]
-        distance = np.zeros((points.shape[0], 1))
+class InstantaneousVelocityExtractor:
+    def __init__(self, mode):
+        """
+        Initilize the mode for velocity extraction.
 
-        if self.mode == 'xy':
-            distance[1:,:] = DistanceExtractor.calculate_2d(
-                points_i_minus1[1:,0:1], 
-                points[1:,0:1],
-                points_i_minus1[1:,1:2], 
-                points[1:,1:2],
-                )
+        Args:
+            mode (str): In 'x' or 'y' or 'xy'.
+        """
+        assert mode in ['x', 'y', 'xy'], "The mode for velocity should be either 'x', 'y' or 'xy'."
+        self.mode = mode
+        self.name = 'velocity_' + mode
+
+
+    @classmethod
+    def calculate(self, displacement, duration):
+        """
+        Calculate the instantanious velocity at a certain point(s).
+
+        Args:
+            displacement (number or 2-D vector): The displacement traveled.
+            duration (number or 2-D vector): The time it took to travel that distance.
+
+        Returns:
+            result (number or 2-D vector): The instantanious velocity when traveling 'distance' during 'time'.
+        """
+        result = displacement/duration
+        return result
+
+
+    def _extract(self, data):
+        """
+        Extract the instantanious velocity from the data dictionary (of one image) and add it to it.
+
+        Args:
+            data (dict): Dictionary with keys as features and their matrices as values.
+        """
+        if data[self.name] is not None:
+            return
+
+        duration_obj = DurationExtractor()
+        if duration_obj.name in data.keys():
+            duration_obj._extract(data)
+            duration = data[duration_obj.name].copy()
         else:
-            distance[1:,:] = DistanceExtractor.calculate_1d(points_i_minus1[1:,:], points[1:,:])
+            data[duration_obj.name] = None
+            duration_obj._extract(data)
+            duration = data[duration_obj.name].copy()
+            data.pop(duration_obj.name)
+
+        displacement_obj = DistanceExtractor(self.mode) if self.mode == "xy" else DisplacementExtractor(self.mode)
+        if displacement_obj.name in data.keys():
+            displacement_obj._extract(data)
+            displacement = data[displacement_obj.name].copy()
+        else:
+            data[displacement_obj.name] = None
+            displacement_obj._extract(data)
+            displacement = data[displacement_obj.name].copy()
+            data.pop(displacement_obj.name)
+
+        velocity = np.zeros(duration.shape)
+        velocity[1:,:] = InstantaneousVelocityExtractor.calculate(displacement[1:,:], duration[1:,:])
             
-        data[self.name] = distance
-        return data
+        data[self.name] = velocity
 
 
 def extract_features(X, extractors):
-    data = {'raw' : X}
+    img = X
+    data = {'raw' : img, DurationExtractor().name: None}
 
     for ext in extractors:
         data[ext.name] = None
 
     for ext in extractors:
-        data = ext._extract(data)
+        ext._extract(data)
+
+    for key in data.copy().keys():
+        if key not in [ext.name for ext in extractors]:
+            data.pop(key)
 
     return data
